cff97207965bf9caea9281dd6462f95cabfb8905
Nizar Arbash <nezar.almohammad@gmail.com>
2023-01-06 14:38:09 +0300

    add support multi controller in Route

--- src/Helper/ApiVersioning.php
+++ src/Helper/ApiVersioning.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Mnarbash\ApiVersioningByHeaderRequest\Helper;
 
-class ApiVersioning{
+class ApiVersioning
+{
     public static function UseApiVersions(array $controller)
     {
         $apiVersioningEnabled = config('api-versioning.api_versioning_enabled');
@@ -24,4 +26,34 @@ class ApiVersioning{
         $controller[0] = str_replace('Controllers\\', "Controllers\\{$folder}{$apiVersion}\\{$apiVersion}", $controller[0]);
         return $controller;
     }
+
+
+    public static function UseApiMultiVersions(array $controllers)
+    {
+        $apiVersioningEnabled = config('api-versioning.api_versioning_enabled');
+
+
+        if (!$apiVersioningEnabled) {
+            return $controllers[0];
+        }
+
+        $apiVersion = app('request')->header('API-VERSION');
+
+        $notDoAnythingWhenVersionIsNotSet = config('api-versioning.not_do_anything_when_version_is_not_set');
+        if ($notDoAnythingWhenVersionIsNotSet && !$apiVersion) {
+            return $controllers[0];
+        }
+
+        if (!isset($controllers[$apiVersion])) {
+            return response()->json([
+                'errors' => ['API version not found for this controller'],
+                'message' => 'API version not found for this controller'],
+                404);
+        }
+
+        return $controllers[$apiVersion];
+
+    }
+
+
 }
